efea8c20fbf8afc787e424f4e6765453836e027c
Roberto <oliveirarobertw@gmail.com>
2025-01-20 10:13:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import streamlit as st
 import pandas as pd
 import plotly as pt
+
+st.set_page_config(layout='wide')
+
+
+df_reviews = pd.read_csv("datasets/customer reviews.csv")
+df_to100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
+
+df_reviews
+df_to100_books
