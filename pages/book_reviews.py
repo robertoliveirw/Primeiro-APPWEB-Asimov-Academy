d22a7652dd469575b07860b93a5ec2f67dc00bda
Roberto <oliveirarobertw@gmail.com>
2025-01-21 11:36:11 -0300

    Update book_reviews.py

--- pages/book_reviews.py
+++ pages/book_reviews.py
@@ -8,3 +8,6 @@ df_to100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
 
 books = df_to100_books['book title'].unique()
 book = st.sidebar.selectbox('Books', books)
+
+df_book = df_to100_books[df_to100_books['book title'] == book]
+df_book
