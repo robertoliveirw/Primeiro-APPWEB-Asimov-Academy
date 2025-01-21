4a6d6ddb26e6a8e6abf4a1bf2e5c56d1b5983493
Roberto <oliveirarobertw@gmail.com>
2025-01-21 11:14:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ st.set_page_config(layout='wide')
 
 df_reviews = pd.read_csv("datasets/customer reviews.csv")
 df_to100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
-
+# Filtrando o valor
 price_max = df_to100_books['book price'].max()
 price_min = df_to100_books['book price'].min() 
-
+#Side bar do valor
 max_price = st.sidebar.slider('Price Range', price_min, price_max, price_max)
 df_books = df_to100_books[df_to100_books['book price'] <= max_price]
 df_books
+#Histograma
+fig = px.bar(df_books['year of publication'].value_counts())
+st.plotly_chart(fig)
