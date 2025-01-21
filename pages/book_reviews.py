57dffe79924ca067fa642a423840a68463708d37
Roberto <oliveirarobertw@gmail.com>
2025-01-21 11:31:29 -0300

    Update book_reviews.py

--- pages/book_reviews.py
+++ pages/book_reviews.py
@@ -1,3 +1,7 @@
 import streamlit as st
+import pandas as pd
 
-st.write('Teste')
+st.set_page_config(layout='wide')
+
+df_reviews = pd.read_csv("datasets/customer reviews.csv")
+df_to100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
