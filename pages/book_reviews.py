3f211a5ffc003b7ee02bea5f609d64d6cc65b7a0
Roberto <oliveirarobertw@gmail.com>
2025-01-21 11:33:55 -0300

    Update book_reviews.py

--- pages/book_reviews.py
+++ pages/book_reviews.py
@@ -5,3 +5,6 @@ st.set_page_config(layout='wide')
 
 df_reviews = pd.read_csv("datasets/customer reviews.csv")
 df_to100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
+
+books = df_to100_books['book title'].unique()
+book = st.sidebar.selectbox('Books', books)
