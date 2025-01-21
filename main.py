21afedc1ce0d91f5abc1310d60511fd458a8f81a
Roberto <oliveirarobertw@gmail.com>
2025-01-21 11:01:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ st.set_page_config(layout='wide')
 df_reviews = pd.read_csv("datasets/customer reviews.csv")
 df_to100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
 
-df_reviews
-df_to100_books
+price_max = df_to100_books['book price'].max()
+price_min = df_to100_books['book price'].min() 
+
+
+max_price = st.sidebar.slider('Price Range', price_min, price_max, price_max)
+
+df_to100_books[df_to100_books['book price'] <= max_price]
