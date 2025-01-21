6ffeedb0b47c045aa64619ac602edae8a4ae14eb
Roberto <oliveirarobertw@gmail.com>
2025-01-21 11:02:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,10 @@ import pandas as pd
 import plotly as pt
 
 st.set_page_config(layout='wide')
-
-
 df_reviews = pd.read_csv("datasets/customer reviews.csv")
 df_to100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
-
 price_max = df_to100_books['book price'].max()
 price_min = df_to100_books['book price'].min() 
-
-
 max_price = st.sidebar.slider('Price Range', price_min, price_max, price_max)
-
-df_to100_books[df_to100_books['book price'] <= max_price]
+df_books = df_to100_books[df_to100_books['book price'] <= max_price]
+df_books
