7385a94984e15cc80aefedcec5746802a8dfe598
Roberto <oliveirarobertw@gmail.com>
2025-01-21 11:49:42 -0300

    Update book_reviews.py

--- pages/book_reviews.py
+++ pages/book_reviews.py
@@ -13,7 +13,15 @@ df_book = df_to100_books[df_to100_books['book title'] == book]
 df_reviews_f = df_reviews[df_reviews['book name'] == book]
 
 book_title = df_book['book title'].iloc[0]
-book_genre = df_book['book genre'].iloc[0]
+book_genre = df_book['genre'].iloc[0]
 book_price = df_book['book price'].iloc[0]
 book_rating = df_book['rating'].iloc[0]
 book_year = df_book['year of publication'].iloc[0]
+
+st.title(book_title)
+st.subheader(book_genre)
+
+col1, col2, col3 = st.columns(3)
+col1.metric('Price', book_price)
+col2.metric('Rating', book_rating)
+col3.metric('Year of Publication', book_year)
